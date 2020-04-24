de33f2eebfe3f74e75667b7faa0be481e6fadd1d
Emerson Pereira <contact@emersonpereira.me>
2020-04-24 00:18:51 -0300

    style menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeaderStyles = styled.header`
+  background: #ccc;
+  display: flex;
+  justify-content: space-between;
+  height: 100px;
+  & > div {
+    /* border: 2px solid #666; */
+  }
+  .menu-button {
+    display: flex;
+    align-items: center;
+    button {
+      background-color: transparent;
+      border: 0;
+      font-size: 30px;
+      margin: 0;
+      padding: 0 20px;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+  .user {
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    p {
+      padding: 0 20px 0 0;
+      font-weight: 500;
+    }
+    img {
+      border-radius: 100%;
+    }
+  }
+`;
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <button>Toggle menu</button>
+    <HeaderStyles>
+      <div className="menu-button">
+        <button>&#9776;</button>
       </div>
-      <div>
-        <p>Mosyle test</p>
+      <div className="user">
+        <p>Emerson Pereira</p>
+        <img
+          src="https://secure.gravatar.com/avatar/1e7bab935d3e724a07874ce45fd22d4b?s=64"
+          alt="Emerson Pereira"
+        />
       </div>
-      <div>
-        <div>
-          <p>Emerson Pereira</p>
-          <img
-            src="https://secure.gravatar.com/avatar/1e7bab935d3e724a07874ce45fd22d4b?s=64"
-            alt="Emerson Pereira"
-          />
-        </div>
-      </div>
-    </header>
+    </HeaderStyles>
   );
 };
 
